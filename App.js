390b6d6ce11e3d592b3eec59a9d8c03e3f11f69d
bellDev-code <sumaoo20@naver.com>
2022-07-07 20:05:41 +0900

    location state

--- App.js
+++ App.js
@@ -1,14 +1,38 @@
-import React from 'react'
+import * as Location from 'expo-location';
+import React, { useEffect, useState } from 'react'
 import { View, StyleSheet, Text, ScrollView, Dimensions } from 'react-native';
 
 const { width: SCREEN_WIDTH } = Dimensions.get('window');
 // const SCREEN_WIDTH = Dimensions.get('window').width
 
 export default function App() {
+  const [city, setCity] = useState("Loading...")
+  const [location, setLocation] = useState(null);
+  const [ok, setOk] = useState(true);
+
+  const ask = async() => {
+    const {granted} = await Location.requestForegroundPermissionsAsync();
+
+    if(!granted) {
+      setOk(false);
+    }
+
+    const {coords: { latitude, longitude }} = await Location.getCurrentPositionAsync({accuracy: 5});
+    const location = await Location.reverseGeocodeAsync(
+      {latitude, longitude}, 
+      {useGoogleMaps: false}
+    );
+    setCity(location[0].city)
+  }
+
+  useEffect(() => {
+    ask();
+  }, [])
+
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView 
         pagingEnabled 
